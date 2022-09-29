28a763662848eaeef194ba020c6ecbc8b8198a50
dgsilveira <dgsilveira@sga.pucminas.br>
2022-09-29 19:28:00 -0300

    feat: Context class

---
+++ FilmesAPI/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Endereco>()
+                .HasOne(endereco => endereco.Cinema)
+                .WithOne(cinema => cinema.Endereco)
+                .HasForeignKey<Cinema>(cinema => cinema.EnderecoId);
+        }
+
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+
+    }
+}
