aa8f2e023ead089ca23e0b0b4609ffedc1267c84
dgsilveira <dgsilveira@sga.pucminas.br>
2022-10-20 00:18:43 -0300

    Chore: Dtos

--- FilmesAPI/Data/Dtos/Cinema/ReadCinemaDto.cs
+++ FilmesAPI/Data/Dtos/Cinema/ReadCinemaDto.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FilmesAPI.Models;
+using System.ComponentModel.DataAnnotations;
 
-namespace FilmesAPI.Data.Dtos.Cinema
+namespace FilmesAPI.Data.Dtos
 {
     public class ReadCinemaDto
     {
@@ -9,5 +10,8 @@ namespace FilmesAPI.Data.Dtos.Cinema
         public int Id { get; set; }
         [Required(ErrorMessage = "O campo de nome é obrigatório")]
         public string Nome { get; set; }
+        public Endereco Endereco { get; set; }
+
+        public Gerente Gerente { get; set; }
     }
 }
